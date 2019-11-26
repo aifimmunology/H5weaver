f92606cefb513a4f42aa2b5ed7fe438685abdddb
Lucas Graybuck <soulcatalyst@gmail.com>
2019-11-26 15:38:38 -0800

    adding additional helper functions to make .h5 files easier to work with; additional tests

--- tests/testthat/test-read_rna_h5.R
+++ tests/testthat/test-read_rna_h5.R
@@ -49,8 +49,61 @@ test_that(
 )
 
 test_that(
+  "read_h5_cell_meta() reads cell metadata from a .h5 file.",
+  {
+    h5_list <- h5dump(test_h5)
+    h5_list <- add_well_metadata(h5_list, "X000-P1W1C1")
+
+    h5_list_meta <- h5_list_cell_metadata(h5_list)
+
+    temp_h5 <- tempfile(pattern = ".h5")
+    write_h5_list(h5_list, temp_h5)
+
+    h5_test_meta <- read_h5_cell_meta(temp_h5)
+
+    expect_true(class(h5_test_meta) == "data.frame")
+    expect_identical(h5_test_meta[, names(h5_list_meta)], h5_list_meta)
+
+  }
+)
+
+test_that(
+  "read_h5_feature_meta() reads feature metadata from a .h5 file.",
+  {
+    h5_list <- h5dump(test_h5)
+    h5_list_meta <- as.data.frame(h5_list$matrix$features[!grepl("^_", names(h5_list$matrix$features))],
+                                  stringsAsFactors = FALSE)
+
+    h5_test_meta <- read_h5_feature_meta(test_h5)
+
+    expect_true(class(h5_test_meta) == "data.frame")
+    expect_identical(h5_test_meta[, names(h5_list_meta)], h5_list_meta)
+  }
+)
+
+test_that(
   "strip_1d_array_recursive() needs tests",
   {
+    h5_list <- rhdf5::h5dump(test_h5)
+    h5_list_matrix_classes <- unlist(lapply(h5_list$matrix, class))
+    h5_list_feature_classes <- unlist(lapply(h5_list$matrix$features, class))
+
+    h5_list_no_array <- strip_1d_array_recursive(h5_list)
+    h5_list_no_array_matrix_classes <- unlist(lapply(h5_list_no_array$matrix, class))
+    h5_list_no_array_feature_classes <- unlist(lapply(h5_list_no_array$matrix$features, class))
+
+    expect_true(class(h5_list_no_array) == class(h5_list))
+    expect_equal(sum(h5_list_no_array_matrix_classes == "list"),
+                 sum(h5_list_matrix_classes == "list"))
+    expect_equal(sum(h5_list_no_array_matrix_classes != "list"),
+                 sum(h5_list_matrix_classes == "array"))
+    expect_equal(sum(h5_list_no_array_matrix_classes == "array"), 0)
+
+    expect_equal(sum(h5_list_no_array_feature_classes == "list"),
+                 sum(h5_list_feature_classes == "list"))
+    expect_equal(sum(h5_list_no_array_feature_classes != "list"),
+                 sum(h5_list_feature_classes == "array"))
+    expect_equal(sum(h5_list_no_array_feature_classes == "array"), 0)
 
   }
 )
@@ -58,6 +111,17 @@ test_that(
 test_that(
   "convert_char_na_recursive() needs tests",
   {
+    h5_list <- h5dump(test_h5)
+    h5_list$matrix$barcodes[sample(1:length(h5_list$matrix$barcodes), 100)] <- "NA"
+    h5_list$matrix$features$name[sample(1:length(h5_list$matrix$features$name), 100)] <- "NA"
+
+    h5_list_na <- convert_char_na_recursive(h5_list)
+
+    expect_true(class(h5_list_na) == class(h5_list))
+    expect_identical(which(is.na(h5_list_na$matrix$barcodes)),
+                     which(h5_list$matrix$barcodes == "NA"))
+    expect_identical(which(is.na(h5_list_na$matrix$features$name)),
+                     which(h5_list$matrix$features$name == "NA"))
 
   }
 )
@@ -65,6 +129,14 @@ test_that(
 test_that(
   "h5_dump() needs tests",
   {
+    rhdf5_h5_list <- rhdf5::h5dump(test_h5)
 
+    fixed_rhdf5_h5_list <- strip_1d_array_recursive(rhdf5_h5_list)
+    fixed_rhdf5_h5_list <- convert_char_na_recursive(fixed_rhdf5_h5_list)
+
+    h5weaver_h5_list <- h5dump(test_h5)
+
+    expect_identical(h5weaver_h5_list,
+                     fixed_rhdf5_h5_list)
   }
 )
